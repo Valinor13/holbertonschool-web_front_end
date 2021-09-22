58e6f52c02e1690d2b81aa93ff0c21ea6172933d
Valinor13 <jwcalhoun2@gmail.com>
2021-09-22 15:12:16 -0500

    Update 9-prime.js

--- 0x00-Javascript_advanced/9-prime.js
+++ 0x00-Javascript_advanced/9-prime.js
@@ -2,7 +2,6 @@ function countPrimeNumbers() {
     return 25;
 }
 
-const timeLog0 = performance.now();
-countPrimeNumbers();
-const timeLog1 = performance.now();
-console.log(`Execution time of printing countPrimeNumbers was ${timeLog1 - timeLog0} milliseconds.`);
+let timeLog = performance.now(countPrimeNumbers());
+
+console.log(`Execution time of printing countPrimeNumbers was ${timeLog} milliseconds.`);
